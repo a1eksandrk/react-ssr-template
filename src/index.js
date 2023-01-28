834166050f8fbe553da3e5dda49c8faf0eed09bb
AlexKomz <a.komzoltsev@gmail.com>
2023-01-28 17:16:46 +0300

    use promises fs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
@@ -16,12 +16,11 @@ app.use(
   )
 )
 
-app.get('/', (_, res) => {
-  fs.readFile(path.resolve("./dist/index.html"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err)
-      return res.status(500).send("An error occurred")
-    }
+app.get('/', async (_, res) => {
+  try {
+    const data = await fs.readFile(
+      path.resolve(process.cwd(), 'src/index.html'), 'utf8'
+    )
 
     return res.send(
       data.replace(
@@ -29,7 +28,10 @@ app.get('/', (_, res) => {
         `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
       )
     )
-  })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send('An error occurred')
+  }
 })
 
 app.listen(port, () => {
